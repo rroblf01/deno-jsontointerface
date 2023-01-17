cd5fe56b56e88b7b17aa1b68d268b96f86d144df
sheik <sheik@pop-os.localdomain>
2023-01-17 16:44:38 +0100

    Cleaned

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,20 +5,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Json to Interface</title>
       </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
+      <main class="bg-gray-500 min-h-screen font-sans">
         <JsonToInterface />
-      </div>
+      </main>
     </>
   );
 }
